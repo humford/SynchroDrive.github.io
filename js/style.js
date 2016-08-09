d6d098ee34762bb3d968cf0ff459714beef2bd20
Ahnaf Taha <taha1120@gmail.com>
2016-08-09 10:57:05 -0400

    aaa
    aaa

--- js/style.js
+++ js/style.js
@@ -1,28 +1,16 @@
-var $animation_elements = $('.animation-element');
-var $window = $(window);
-
-$window.on('scroll', check_if_in_view);
-$window.on('scroll resize', check_if_in_view);
-
-$window.trigger('scroll');
-
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
-
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
+function geo_success(position) {
+  do_something(position.coords.latitude, position.coords.longitude);
 }
+
+function geo_error() {
+  alert("Sorry, no position available.");
+}
+
+var geo_options = {
+  enableHighAccuracy: true,
+  maximumAge        : 30000,
+  timeout           : 27000
+};
+
+var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+
