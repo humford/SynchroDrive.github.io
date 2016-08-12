52f980fbb70eb2a8353a5d0367ed408b6128f93b
Ahnaf Taha <AhnafTaha@Ahnafs-MacBook-Pro.local>
2016-08-11 22:35:01 -0400

    hi
    how are you bro

--- js/app-script.js
+++ js/app-script.js
@@ -5,7 +5,7 @@ var locationCenter;
 var myCircle;
 
 function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 	  center: mapCenter,
 	  zoom: 6
 	});
