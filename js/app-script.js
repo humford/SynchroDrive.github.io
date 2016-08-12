90482ee34ba1783c84e1bcf2d2c5686952b8895f
Ahnaf Taha <AhnafTaha@Ahnafs-MacBook-Pro.local>
2016-08-11 22:02:21 -0400

    aaa
    aaa

--- js/app-script.js
+++ js/app-script.js
@@ -1,129 +1,23 @@
+var map;
+var mapCenter = {lat: -34.397, lng: 150.644};
+var tweets = [];
+var locationCenter;
+
 function initMap() {
-	var pos;
 	if (navigator.geolocation) {
-  		console.log('Geolocation is supported!');
 
 		navigator.geolocation.getCurrentPosition(function(position) {
-            pos = {
+            mapCenter = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
-				var marker = new google.maps.Marker({
-    			position: pos,
-    			map: map,
-    			title: 'Hello World!'
-  			});
-          });
-	}
-	else {
-  		console.log('Geolocation is not supported for this Browser/OS version yet.');
-	}
+		});
+    } else {
+        alert("You don't support this");
+    }
 
 	var map = new google.maps.Map(document.getElementById('map'), {
-    center: pos,
-	zoom: 6,
-	styles:[
-				{
-					"featureType": "administrative",
-			        "elementType": "labels.text.fill",
-			        "stylers": [
-			            {
-			                "color": "#444444"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "landscape",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "color": "#f2f2f2"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "poi",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "visibility": "off"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "road",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "saturation": -100
-			            },
-			            {
-			                "lightness": 45
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "road.highway",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "visibility": "simplified"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "road.arterial",
-			        "elementType": "labels.icon",
-			        "stylers": [
-			            {
-			                "visibility": "off"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "transit",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "visibility": "off"
-			            }
-			        ]
-			    },
-			    {
-			        "featureType": "water",
-			        "elementType": "all",
-			        "stylers": [
-			            {
-			                "color": "#B2EDFF"
-			            },
-			            {
-			                "visibility": "on"
-			            }
-			        ]
-			    }
-			]});
-}
-
-
-function findUser() {
-	if (navigator.geolocation) {
-  		console.log('Geolocation is supported!');
-
-		navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-				var marker = new google.maps.Marker({
-    			position: pos,
-    			map: map,
-    			title: 'Hello World!'
-  			});
-          });
-	}
-	else {
-  		console.log('Geolocation is not supported for this Browser/OS version yet.');
-	}
+	  center: mapCenter,
+	  zoom: 6
+	});
 }
