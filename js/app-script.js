fe3fef2352ded311728442f1807de1b0d08b5236
Ahnaf Taha <AhnafTaha@Ahnafs-MacBook-Pro.local>
2016-08-11 22:29:51 -0400

    aaaa
    update

--- js/app-script.js
+++ js/app-script.js
@@ -2,6 +2,7 @@ var map;
 var mapCenter = {lat: -34.397, lng: 150.644};
 var tweets = [];
 var locationCenter;
+var myCircle;
 
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -9,6 +10,14 @@ function initMap() {
 	  zoom: 6
 	});
 
+	checkLocation();
+};
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	console.log("Couldn't do this!");
+};
+
+function checkLocation() {
 	if (navigator.geolocation) {
 
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -17,17 +26,24 @@ function initMap() {
               lng: position.coords.longitude
             };
 
-			map.setCenter(locationCenter);
+			map.panTo(locationCenter);
 
 			console.log(locationCenter);
+
+			myCircle = new google.maps.Circle({
+            	strokeColor: '#FF0000',
+            	strokeOpacity: 0.8,
+            	strokeWeight: 2,
+            	fillColor: '#FF0000',
+            	fillOpacity: 0.35,
+            	map: map,
+            	center: locationCenter,
+            	radius: 10
+          	});
 		}, function() {
 			console.log("wtf bro");
 		});
     } else {
         alert("You don't support this");
     }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	console.log("Couldn't do this!");
-}
+};
