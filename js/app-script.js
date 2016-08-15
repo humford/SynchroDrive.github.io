210b2d68883ad0327b7a7e808bfbb47ce22d759e
Ahnaf Taha <AhnafTaha@Ahnafs-MBP.mshome.net>
2016-08-15 15:13:19 -0400

    fix
    now

--- js/app-script.js
+++ js/app-script.js
@@ -1,4 +1,5 @@
-var mapCenter = [];
+var mapCenter;
+var mapPolygon;
 
 var map = new GMaps({
 	el: '#map',
@@ -6,33 +7,25 @@ var map = new GMaps({
 	lng: -77
 });
 
-function checkLocation(){
+function checkLocation() {
 	GMaps.geolocate({
-	  success: function(position) {
+		success: function(position) {
 
-	    map.setCenter(position.coords.latitude, position.coords.longitude);
+			mapCenter = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			}
 
-		mapCenter = [position.coords.latitude, position.coords.longtitude];
-
-		var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: mapCenter,
-            radius: 100
-          });
-	  },
-	  error: function(error) {
-	    alert('Geolocation failed: '+error.message);
-	  },
-	  not_supported: function() {
-	    alert("Your browser does not support geolocation");
-	  },
-	  always: function() {
-	    alert("Done!");
-	  }
+	    	map.setCenter(mapCenter);
+		},
+	  	error: function(error) {
+	    	alert('Geolocation failed: '+error.message);
+		},
+	  	not_supported: function() {
+			alert("Your browser does not support geolocation");
+		},
+		always: function() {
+	    	alert("Done!");
+		}
 	});
 };
