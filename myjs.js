9bc4ea1c4bc28e5bc6244719fa2ed4c35f018ae4
Ahnaf Taha <AhnafTaha@Ahnafs-MBP.mshome.net>
2016-08-15 12:38:08 -0400

    try
    try man living legend i feel like kobe

--- myjs.js
+++ myjs.js
@@ -3,8 +3,15 @@ var mapCenter = [34.397, 150.644];
 
 function initMap() {
     var createdOrigin = new google.maps.LatLng(mapCenter);
-map = new google.maps.Map(document.getElementById('map'), {
-  center: createdOrigin,
-  zoom: 16
-});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: createdOrigin,
+        zoom: 16
+    });
+
+    var createdOrigin = new google.maps.LatLng(mapCenter);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: createdOrigin,
+        zoom: 16
+    });
+
 };
